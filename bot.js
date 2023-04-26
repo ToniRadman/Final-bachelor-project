d79d7b449f9b113b14f15c56fef6bccff34b57a0
Toni Radman <toniradman2903@gmail.com>
2023-04-26 17:50:29 +0200

    Discord bot setup

---
+++ bot.js
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+const openai = new OpenAIApi(new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+);
+
+client.on("messageCreate", async function (message) {
+    if (message.author.bot) return;
+    
+    try {
+      const response = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [
+              {role: "system", content: "You are a helpful assistant who responds succinctly"},
+              {role: "user", content: message.content}
+          ],
+        });
+  
+      const content = response.data.choices[0].message;
+      return message.reply(content);
+  
+    } catch (err) {
+      return message.reply(
+        "As an AI robot, I errored out."
+      );
+    }
+  });
+
+  client.login(process.env.DISCORD_BOT_TOKEN);
