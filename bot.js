cd7171a5b1d12d2ce0009df7883b13e3c013bdf1
Toni Radman <toniradman2903@gmail.com>
2023-05-31 18:01:14 +0200

    interaction limit(spam prevention) + comments

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
-import { Client, GatewayIntentBits } from "discord.js";
-import { Configuration, OpenAIApi } from "openai";
-import { schedule } from "node-cron";
+import dotenv from 'dotenv';
+import { Client, GatewayIntentBits } from 'discord.js';
+import { Configuration, OpenAIApi } from 'openai';
+import { schedule } from 'node-cron';
 //import is being used instead of require due to syntax reasons
 
 dotenv.config();
@@ -17,8 +17,8 @@ const client = new Client({
 });
 //bot initiation with its privileges/intents
 
-client.on("ready", () =>{
-  console.log("The AI bot is online");
+client.on('ready', () =>{
+  console.log('The AI bot is online');
 });
 //bot's online status check-up
 
@@ -32,30 +32,33 @@ schedule('*/5 * * * *', () => {
   //node-cron schedule function executes the code bellow every day after its first initiation
 
   client.login(process.env.DISCORD_BOT_TOKEN);
-  //bot's API key in .env file is being read through "process" property for connecting/login
+  //bot's API key in .env file is being read through "process" property for connecting and logging in
+  //it prevents bot going permanently offline since it can logout itself in case of inactivity which may
+  //be potentially caused by the last setTimeout function since it disables bot's functionality
+  //temporarily(lines 80-83)
 
   setTimeout(() => {
     //the function delays execution of the code bellow so the login function above can finish its
-    //execution properly, otherwise only console.log is going to be executed properly afterwards
+    //execution properly, otherwise only the next console.log function is going to be executed afterwards
 
-    console.log('running a task every minute');
-    //schedule function trigger check-up
+    console.log('running a task every day');
+    //schedule function check-up
 
     const channel = client.channels.cache.find(channel => channel.name === 'general');
-    channel.send('Hi! I am a discord bot which provides a new word every day in any language you choose:');
+    channel.send('Hi! I am an AI discord bot which explains a new word every day in any language you choose:');
     //bot sends initial message to filtered channel as in-app schedule function check up
     //also it is a sign that bot can be used
     
-    client.on("messageCreate", async function (message) {
+    client.on('messageCreate', async function (message) {
       if (message.author.bot) return;
-      //if statement checks if any bot sent a message so it could be ignored 
+      //if statement checks if any bot sent a message so it can be ignored 
     
       try {
         const response = await openai.createChatCompletion({
-          model: "gpt-3.5-turbo",
+          model: 'gpt-3.5-turbo',
           messages: [
-              {role: "system", content: "Pick a random word and describe it in chosen language"},
-              {role: "user", content: message.content}
+              {role: 'system', content: 'Pick a random non-repetitive word and describe it in chosen language'},
+              {role: 'user', content: message.content}
           ],
         });
   
@@ -65,15 +68,23 @@ schedule('*/5 * * * *', () => {
         //on the context given in "content" property, it creates a response which is being sent back
   
       } catch (err) {
-        return message.reply(
-          "As an AI robot, I errored out."
-        );
+        return message.reply('As an AI robot, I errored out.');
       }
       //the bot throws an error message in lack of proper response to the request or can't process some
       //of them in a short period if there are multiple ones
 
     });
+
+    setTimeout(() => {
+      channel.send('I am sorry for being unable to talk anymore, but see you tomorrow. Have a nice day!');
+      client.removeAllListeners('messageCreate');
+      console.log('Stopped receiving messages.');
+    }, 60000);
+    //even though the bot remains online(mostly), it will stop receiving messages 1 minute after the
+    //previous setTimeout activation to prevent potential bot spamming and overload by destroying
+    //"messageCreate" event listener which bot uses for receiving messages
+
   }, 3000);
   //setTimeout is being delayed for 3 seconds
-  
+
 });
