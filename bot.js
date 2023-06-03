4f15aab971dd2d97319f00ddf12b9d8ab425cc2c
Toni Radman <toniradman2903@gmail.com>
2023-06-03 21:17:09 +0200

    Updated comments and content prop inside AI func.

--- bot.js
+++ bot.js
@@ -17,35 +17,35 @@ const client = new Client({
 });
 //bot initiation with its privileges/intents
 
-client.on('ready', () =>{
-  console.log('The AI bot is online');
-});
-//bot's online status check-up
-
 const openai = new OpenAIApi(new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   })
 );
 //OpenAI's API configuration
 
-schedule('*/5 * * * *', () => {
+schedule('*/2 * * * *', () => {
   //node-cron schedule function executes the code bellow every day after its first initiation
 
   client.login(process.env.DISCORD_BOT_TOKEN);
   //bot's API key in .env file is being read through "process" property for connecting and logging in
   //it prevents bot going permanently offline since it can logout itself in case of inactivity which may
-  //be potentially caused by the last setTimeout function since it disables bot's functionality
-  //temporarily(lines 78-82)
+  //be potentially caused by the setTimeout function since it disables bot's functionality
+  //temporarily(lines 93-97)
 
-  setTimeout(() => {
+  client.on('ready', () =>{
+    console.log('The AI bot is online');
+    //bot's online status check-up
+  });
+
+  setTimeout(async () => {
     //the function delays execution of the code bellow so the login function above can finish its
-    //execution properly, otherwise only the next console.log function is going to be executed afterwards
+    //execution properly
 
     console.log('running a task every day');
     //schedule function check-up
 
     const channel = client.channels.cache.find(channel => channel.name === 'general');
-    channel.send('Hi! I\'m an AI discord bot which explains a new word every day in any language you choose:');
+    await channel.send('Hi! I\'m an AI discord bot which explains a new word every day in any language you choose:');
     //bot sends initial message to filtered channel as in-app schedule function check up
     //also it is a sign that bot can be used
     
@@ -57,8 +57,24 @@ schedule('*/5 * * * *', () => {
         const response = await openai.createChatCompletion({
           model: 'gpt-3.5-turbo',
           messages: [
-              {role: 'system', content: 'Pick a random non-repetitive word and describe it in chosen language'},
-              {role: 'user', content: message.content}
+              {role: 'user', content: message.content},
+              {role: 'system', content: 
+              `Generate a new random word in the chosen language and provide its description in the following format:
+
+              Word: [Word]
+              Language: ${message.content}
+              
+              Meaning: [Description of the meaning]
+              Pronunciation: [Pronunciation guide or phonetic transcription]
+              
+              Additional Information: [Any relevant additional information or context]
+              
+              Translation into ${message.content}:
+              [Translation of the entire description into the ${message.content}, except for the English language]
+              
+              Please ensure that the description adheres to this format, including the word, language, meaning, and pronunciation. 
+              Also you must accept only language names as valid inputs. Thank you!`
+              }
           ],
         });
   
@@ -68,23 +84,23 @@ schedule('*/5 * * * *', () => {
         //on the context given in "content" property, it creates a response which is being sent back
   
       } catch (err) {
-        return message.reply('As an AI bot, I errored out.');
+        return message.reply('As an AI bot, I encountered an error.');
       }
       //the bot throws an error message in lack of proper response to the request or can't process some
       //of them in a short period if there are multiple ones
 
     });
 
-    setTimeout(() => {
-      channel.send('I\'m sorry that we can\'t talk anymore, but see you tomorrow. Have a nice day!');
+    setTimeout(async () => {
+      await channel.send('I\'m sorry that we can\'t talk anymore, but see you tomorrow. Have a nice day!');
       client.removeAllListeners('messageCreate');
       console.log('Stopped receiving messages.');
     }, 60000);
     //even though the bot remains online(mostly), it will stop receiving messages 1 minute after the
     //previous setTimeout activation to prevent potential bot spamming and overload by destroying
     //"messageCreate" event listener which bot uses for receiving messages
-
+  
   }, 3000);
-  //setTimeout is being delayed for 3 seconds
+  //setTimeout function delay is 3 seconds
 
 });
